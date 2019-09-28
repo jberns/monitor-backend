6e4bc405dc78376d129dcd3e2c9031d4c4a1d6b9
Reaper <richiereper@gmail.com>
2019-09-28 16:01:05 -0600

    Update JSONtoSQLServer.py

--- JSONtoSQLServer.py
+++ JSONtoSQLServer.py
@@ -6,13 +6,15 @@ HeartLink
 """
 
 import sqlite3
-import datetime
 conn = sqlite3.connect("HeartLink_DB.db")
-while True:
-    TimeVar = str(datetime.datetime.now())
+
+
+def send_to_server(input_dict):
     conn.execute('''
         INSERT INTO FR_DATA (UID, TS, HR, BP_D, BP_S, LAT, LON, A_SUP, ENV_TEMP, INT_TEMP, FNAME, LNAME) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-        ''', (0, TimeVar[0:23], 80, 0, 0, 0, 0, 0, 0, 0, 'Jon', 'Smith'))
+        ''', (input_dict['UID'], input_dict['TS'], input_dict['HR'], input_dict['BP_D'], input_dict['BP_S'], input_dict['LAT'], input_dict['LON'], input_dict['A_SUP'], input_dict['ENV_TEMP'], input_dict['INT_TEMP'], input_dict['FNAME'], input_dict['LNAME']))
     conn.commit()
-    exit()
-conn.close()
+    conn.close()
+
+def main():
+    
