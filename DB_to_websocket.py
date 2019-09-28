31379bc6cc110f90e7f316dc4896e49c3769ff44
happyklans <bstanton4@student.cccs.edu>
2019-09-28 14:38:46 -0600

    converted from tuple to dictionary for serialization

--- DB_to_websocket.py
+++ DB_to_websocket.py
@@ -26,11 +26,14 @@ def getDataSnapshot():
     return records
 
 def serializeDataSnapshot(records):
-
+    columns = ["UID", "TS","HR", "BP_D", "BP_S", "LAT", "LON", "A_SUP", "ENV_TEMP", "IN_TEMP", "FNAME", "LNAME"]
+    record_dict = {}
     packet = []
 
     for record in records:
-        packet.append(js.dumps(record))
+        for i in range(len(columns)):
+            record_dict[columns[i]] = record[i]
+        packet.append(js.dumps(record_dict))
 
     print(packet)
 
